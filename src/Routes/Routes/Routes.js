2c1eeb8ef250e4592a8672a782d2362adfffa64f
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-12-24 15:10:32 +0600

    navbar setup

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Appointment from "../../Pages/Appointment/Appointment";
+import AboutUs from "../../Pages/Home/AboutUs/AboutUs";
 import Home from "../../Pages/Home/Home/Home";
+import Reviews from "../../Pages/Home/Reviews/Reviews";
 import Login from "../../Pages/login/Login";
 
 const router = createBrowserRouter([
@@ -15,6 +18,18 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/about-us',
+                element: <AboutUs></AboutUs>
+            },
+            {
+                path: '/reviews',
+                element: <Reviews></Reviews>
+            },
+            {
+                path: '/appointment',
+                element: <Appointment></Appointment>
             }
         ]
     }
