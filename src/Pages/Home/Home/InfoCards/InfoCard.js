13fab8ecfcd5e14f14bab268c858dc50428dfc2e
hridoyhalder1 <hridoyhalder91@gmail.com>
2023-01-09 00:01:43 +0600

    cardInfo added

--- src/Pages/Home/Home/InfoCards/InfoCard.js
+++ src/Pages/Home/Home/InfoCards/InfoCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const InfoCard = () => {
+const InfoCard = ({card}) => {
+    const {name, icon, description, bgClass} = card;
     return (
-        <div>
-            
+        <div className={`card card-side p-6  shadow-xl ${bgClass} text-white`}>
+            <figure><img src={icon} alt="Movie" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{description}</p>
+                
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home/InfoCards/InfoCards.js
+++ src/Pages/Home/Home/InfoCards/InfoCards.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import clock from '../../../../assets/icons/clock.svg';
+import location from '../../../../assets/icons/marker.svg';
+import phone from '../../../../assets/icons/phone.svg';
+import InfoCard from './InfoCard';
 
 const InfoCards = () => {
+
+    const cardData = [
+        {
+            id:1,
+            name: 'Opening Hours',
+            description: 'Open 10.00 Am to 6 PM',
+            icon: clock,
+            bgClass: 'bg-primary'
+        },
+        {
+            id:2,
+            name: 'Visit Our Location',
+            description: 'Brooklyn, NY 10036, United States',
+            icon: location,
+            bgClass: 'bg-accent'
+        },
+        {
+            id:3,
+            name: 'Contact us now',
+            description: '+000 123 456789',
+            icon: phone,
+            bgClass: 'bg-primary'
+        },
+    ]
+
     return (
-        <div>
-            
+        <div className=' grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-14 mb-14 gap-6'>
+            {
+                cardData.map(card=> <InfoCard
+                key={card.id}
+                card={card}
+                >
+
+                </InfoCard>)
+            }
         </div>
     );
 };
